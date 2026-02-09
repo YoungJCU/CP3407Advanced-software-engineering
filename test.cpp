f05869531b383c4ef45f3c3e41a561300e64fcd6
chongyuang <yuang.chong@my.jcu.edu.au>
2026-02-09 11:49:54 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 崇宇昂 on 2026/2/9.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by 崇宇昂 on 2026/2/9.
+//
+
+#ifndef 未命名文件夹_TEST_H
+#define 未命名文件夹_TEST_H
+
+
+class test
+{
+};
+
+
+#endif //未命名文件夹_TEST_H
